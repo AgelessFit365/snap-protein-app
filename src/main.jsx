a8c377f3a4f6da2f9d20863979c96b8b2fc82f8c
Robert Fowler <agelessfitness@protonmail.com>
2026-03-20 22:39:44 -0500

    Add PostHog protein and referral tracking

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,19 @@ posthog.init('phc_GZwTxzMsbs9ggCpdMU837wp5NKWAWTRV4pQRDv7vbiH', {
   api_host: 'https://us.i.posthog.com',
 })
 
+const params = new URLSearchParams(window.location.search);
+const ref = params.get('ref');
+
+if (ref) {
+posthog.register({
+  referrer: ref,
+});
+
+posthog.capture('referral_detected', {
+  referrer: ref,
+});  
+}
+
 posthog.capture('beta_app_opened')
 
 createRoot(document.getElementById('root')).render(
